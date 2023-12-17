0f0d4b2041248c3600d7cc12b698bcab6035e2b9
breno.mcosta <breno.mcosta@pbh.gov.br>
2023-12-17 16:28:13 -0300

    init

---
+++ src/main/java/com/br/awslistener/AwsListenerApplication.java
@@ -0,0 +1,13 @@
+package com.br.awslistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/awslistener/AwsListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.awslistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
